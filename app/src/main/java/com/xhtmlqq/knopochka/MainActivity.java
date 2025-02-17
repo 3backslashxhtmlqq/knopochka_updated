1338dbda7a8664480d97e5008862807d087f1b9c
3backslashxhtmlqq <kybakaytz@gmail.com>
2025-02-17 23:32:37 +0300

    updated

---
+++ app/src/main/java/com/xhtmlqq/knopochka/MainActivity.java
@@ -0,0 +1,46 @@
+package com.xhtmlqq.knopochka;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Integer counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt("counter", 0);
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+    }
+
+    public void onClick(View view) {
+        counter++;
+        if (counter == 1337) {
+            finish();
+        }
+        TextView onClickText = findViewById(R.id.textView2);
+        onClickText.setText("Вы накликали: " + counter.toString() + " коробок");
+    }
+}
